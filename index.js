8b1ed9e4b7404dfaf28afb1d37a196292ed248d3
Pawantyagi123 <pawantyaginnl2002@gmail.com>
2023-12-02 17:36:54 +0530

    changes

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 
+const collapse = document.querySelector(".collapse navbar-collapse");
+const navitem = document.querySelector(".nav-item active")
+collapse.addEventListener("click", () =>{
+ console.log(navitem);
+});
