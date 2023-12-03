46c0ca122d99e942a5c7975ab3f336d993630168
Pawantyagi123 <pawantyaginnl2002@gmail.com>
2023-12-03 13:52:57 +0530

    add changes

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
+const form = document.querySelector("form");
+const fullname = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+function sendEmail () {
+const bodyMessage = `Full Name: ${fullname.value} <br> Email: ${email.value} <br> Phone number: ${phone.value} <br> Message: ${message.value}`;
 
-const collapse = document.querySelector(".collapse navbar-collapse");
-const navitem = document.querySelector(".nav-item active")
-collapse.addEventListener("click", () =>{
- console.log(navitem);
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "pawantyaginnl2002@gmail.com",
+        Password : "987B0614E5F9AD530FC3B7A9A6DC372563E5",
+        To : 'pawantyaginnl2002@gmail.com',
+        From : "pawantyaginnl2002@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+      message => {
+        if (message == "ok"){
+          Swal.fire({
+            title: "Success!",
+            text: "Message sent succesfully!",
+            icon: "success"
+          });
+        }
+      }
+    );
+};
+
+form.addEventListener("submit", (e) =>{
+   e.preventDefault();
+
+   sendEmail ();
 });
