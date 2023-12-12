527ecb1ef1ecd244284315224d0996cde999a4d2
Pawantyagi123 <pawantyaginnl2002@gmail.com>
2023-12-12 11:45:47 +0530

    changes

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const form = document.querySelector("form");
-const fullname = document.getElementById("name");
+const fullName = document.getElementById("name");
 const email = document.getElementById("email");
 const phone = document.getElementById("phone");
 const subject = document.getElementById("subject");
-const message = document.getElementById("message");
+const messa = document.getElementById("message");
 function sendEmail () {
-  const bodyMessage = `Full Name: ${fullname.value} <br> Email: ${email.value} <br> Phone number: ${phone.value} <br> Message: ${message.value}`;
-
-  Email.send({
-    Host : "smtp.elasticemail.com",
+  const bodyMessage = `Fullname: ${fullName.value}<br> Email: ${email.value}<br> Phone number: ${phone.vlaue}<br> Subject: ${subject.value}<br> Message:${messa.value}`
+  email.send({
+    Host : "smtp.gmail.com",
     Username : "pawantyaginnl2002@gmail.com",
     Password : "987B0614E5F9AD530FC3B7A9A6DC372563E5",
     To : 'pawantyaginnl2002@gmail.com',
-    From : "pawantyaginnl2002@gmail.com",
+    From : "you@isp.com",
     Subject : subject.value,
     Body : bodyMessage
   }).then(
     message => {
-      if (message == "ok"){
+      if (message === "OK"){
         Swal.fire({
           title: "Success!",
           text: "Message sent successfully!",
           icon: "success"
-          });
-        }
+        });
       }
-    );
-};
+    }
+  );
+}
+
 function checkInputs() {
   const items = document.querySelectorAll(".item");
 
@@ -60,7 +60,7 @@ function checkInputs() {
 function checkEmail() {
   const emailRegex = /^([a-z\d\.-]+)@([a-z\d.-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
 
-  const errorTxtEmail = document.querySelector(".error-text email");
+  const errorTxtEmail = document.querySelector(".error-text.email");
 
   if (!email.value.match(emailRegex)) {
     email.classList.add("error");
@@ -82,9 +82,12 @@ function checkEmail() {
 form.addEventListener("submit", (e) =>{
   e.preventDefault();
   checkInputs();
-
-  if (!fullname.classList.contains("error") && !email.classList.contains("error") && !phone.classList.contains("error") && !subject.classList.contains("error") && !message.classList.contains("error")) {
+ 
+  if (!fullName.classList.contains("error") && !email.classList.contains("error") && !phone.classList.contains("error") && !subject.classList.contains("error") && !messa.classList.contains("error")) {
     sendEmail ();
+
+    form.reset();
+    return false;
   }
   
 });
